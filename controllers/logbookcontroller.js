f1f8dd7dd6e88cfef7c92f39197d8cd2496ca0ce
Telma Chapa <telchapa@gmail.com>
2021-08-18 20:41:35 -0400

    last update

--- controllers/logbookcontroller.js
+++ controllers/logbookcontroller.js
@@ -2,8 +2,9 @@ let express = require('express');
 let router = express.Router();
 let validateSession = require('../middleware/validate-session');
 const Logbook = require('../db').import('../models/loogbook');
-
-
+///////////////////////////
+// Creates a Workout log///
+//////////////////////////
 router.post('/', validateSession, (req, res) => {
     const logbookEntry = {
         description: req.body.log.description,
@@ -16,12 +17,18 @@ router.post('/', validateSession, (req, res) => {
         .catch(err => res.status(500).json({ error: err }))
 })
 
+///////////////////////////////////////////////
+// Gets all the workout logs from all users////
+///////////////////////////////////////////////
 // router.get("/all", (req, res) => {
 //     Logbook.findAll()
 //         .then(logbooks => res.status(200).json(logbooks))
 //         .catch(err => res.status(500).json({ error: err }))
 // });
 
+////////////////////////////////////////////////////////////////
+// Gets the workout logs from a specific user by the owner_id///
+////////////////////////////////////////////////////////////////
 router.get('/', validateSession, (req, res) => {
     let userid = req.user.id
     Logbook.findAll({
@@ -31,6 +38,9 @@ router.get('/', validateSession, (req, res) => {
         .catch(err => res.status(500).json({ error: err }))
 });
 
+/////////////////////////////////
+// Gets a workout log by title///
+/////////////////////////////////
 // router.get('/:title', function (req, res) {    
 //     let title = req.params.title;
 
@@ -41,6 +51,9 @@ router.get('/', validateSession, (req, res) => {
 //         .catch(err => res.status(500).json({ error: err }))
 // });
 
+////////////////////////////////
+//Updates a Workoutlog By ID ///
+////////////////////////////////
 router.put("/:id", validateSession, function (req, res) {
     const updateLogbookEntry = {
         description: req.body.log.description,
@@ -55,6 +68,9 @@ router.put("/:id", validateSession, function (req, res) {
         .catch((err) => res.status(500).json({ error: err }));
 })
 
+//////////////////////////////////
+// Deletes a Workout log By ID///
+/////////////////////////////////
 router.delete("/:id", validateSession, function (req, res) {
     const query = { where: { id: req.params.id, owner_id: req.user.id } };
 
